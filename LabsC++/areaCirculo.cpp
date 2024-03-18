702a748aa8a0e89b96ec54b495a8314eb9857a0f
Alayna Vazquez <alayna.vazquez3@gmail.com>
2024-03-18 09:24:53 -0400

    "New"

--- LabsC++/areaCirculo.cpp
+++ LabsC++/areaCirculo.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 
 int main()
-
+{
 //Definicion de variables
     double area;
     double radio; // Es lo mismo que area, radio;
-    const double PI = 3.14  // Identificador de constante
+    const double PI = 3.14;  // Identificador de constante
 
-    count <<"Ingrese la medida del radio: ";
+    cout <<"Ingrese la medida del radio: ";
     cin >> radio;
 
     area = PI * radio * radio;
 
-    count <<"El area del circulo es: "<<area <<end;
+    cout <<"El area del circulo es: "<<area <<endl;
 
     return 0;
+}
